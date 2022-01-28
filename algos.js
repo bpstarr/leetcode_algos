d6898be5d41ebfb05a8c3987acf2f32e34e11041
bpstarr <blakepstarr@gmail.com>
2022-01-27 20:41:21 -0600

    Inserting day 3 and 1 of algos

--- algos.js
+++ algos.js
@@ -16,3 +16,28 @@ var rotate = function(nums, k) {
         nums.unshift(shifted)
     }
 };
+
+// Move 0 values to the end of the array
+
+var moveZeroes = function(nums) {
+    let lastZero =0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            [nums[lastZero],nums[i]] = [nums[i],nums[lastZero]];
+            lastZero++;
+        }
+    }
+    return nums;
+};
+
+// If two numbers added together = the target then return the two numbers in an array
+
+var twoSum = function(numbers, target) {
+    let leftPointer = 0
+    let rightPointer = numbers.length - 1
+    while( numbers[leftPointer] + numbers[rightPointer] !== target ) {
+        if(numbers[leftPointer] + numbers[rightPointer] > target) rightPointer--
+        else leftPointer++
+    }
+    return [leftPointer+1, rightPointer+1]
+};
